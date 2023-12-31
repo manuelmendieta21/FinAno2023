5982a5e23361ac98c42ce35c730a0b45c321004d
Manuel Andres Mendieta <46815715+manuelmendieta21@users.noreply.github.com>
2023-12-31 17:02:15 -0500

    Actualiza
    contra tiempo sin pull request

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Comprobar si el temporizador ha llegado a cero
         if (distance < 0) {
             clearInterval(countdownInterval);
-            timerElement.innerHTML = '¡Feliz Año 2024!';
+            timerElement.innerHTML = '¡Feliz Año! ';
         
             // Insertar el código HTML adicional con las clases "firework"
             const fireworksContainer = document.createElement('div');
@@ -34,18 +34,50 @@ document.addEventListener('DOMContentLoaded', function () {
             // Agregar el contenedor de fuegos artificiales al cuerpo del documento
             document.body.appendChild(fireworksContainer);
 
+            // Insertar la etiqueta de audio (primer audio)
+            const audioElement1 = document.createElement('audio');
+            audioElement1.autoplay = true;
+            audioElement1.loop = true;
 
-            // Insertar la etiqueta de audio
-            const audioElement = document.createElement('audio');
-            audioElement.autoplay = true;
-            audioElement.loop = true;
+            const audioSource1 = document.createElement('source');
+            audioSource1.src = 'sounds/SoundArt.mp3';
+            audioSource1.type = 'audio/mp3';
 
-            const audioSource = document.createElement('source');
-            audioSource.src = 'sounds/SoundArt.mp3';
-            audioSource.type = 'audio/mp3';
+            audioElement1.appendChild(audioSource1);
+            document.body.appendChild(audioElement1);
 
-            audioElement.appendChild(audioSource);
-            document.body.appendChild(audioElement);
+            // Insertar la etiqueta de audio (segundo audio)
+            const audioElement2 = document.createElement('audio');
+            audioElement2.autoplay = true;
+            audioElement2.loop = true;
+
+            const audioSource2 = document.createElement('source');
+            audioSource2.src = 'sounds/anonuevo.mp3';  // Reemplaza con la ruta de tu segundo archivo de audio
+            audioSource2.type = 'audio/mp3';
+
+            audioElement2.appendChild(audioSource2);
+            document.body.appendChild(audioElement2);
+
+            // Cambiar el contenido del elemento h1
+            const yearDisplay = document.getElementById('yearDisplay');
+            yearDisplay.innerHTML = '2024';
+
+             // Actualizar el reloj cada segundo
+            const clockInterval = setInterval(function () {
+                const currentDateTime = new Date();
+                const hours = currentDateTime.getHours();
+                const minutes = currentDateTime.getMinutes();
+                const seconds = currentDateTime.getSeconds();
+
+                // Formatear la hora para asegurarse de que siempre tenga dos dígitos
+                const formattedHours = hours < 10 ? `0${hours}` : hours;
+                const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+                const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+                // Mostrar la hora en el elemento con id "clock"
+                const clockElement = document.getElementById('clock');
+                clockElement.innerHTML = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+            }, 1000);
         }
     }, 1000);
 });
